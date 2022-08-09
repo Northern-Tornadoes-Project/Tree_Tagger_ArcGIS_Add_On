d0210b5ca2b0e595fc17981f163ee3f3ae67d10f
Daniel Butt <47188958+Daniel-Butt@users.noreply.github.com>
2022-08-09 10:33:03 -0400

    Add files via upload

---
+++ TreeTaggerModule/ShowTreeTagSelectionWindow.cs
@@ -0,0 +1,47 @@
+﻿/* Mostly boiler plate for setuping up settings window
+ * 
+ * 
+ * 
+ */
+
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeTaggerModule
+{
+    internal class ShowTreeTagSelectionWindow : Button
+    {
+
+        private TreeTagSelectionWindow _treetagselectionwindow = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_treetagselectionwindow != null)
+                return;
+            _treetagselectionwindow = new TreeTagSelectionWindow(null);
+            _treetagselectionwindow.Owner = FrameworkApplication.Current.MainWindow;
+            _treetagselectionwindow.Closed += (o, e) => { _treetagselectionwindow = null; };
+            _treetagselectionwindow.Show();
+            //uncomment for modal
+            //_treetagselectionwindow.ShowDialog();
+        }
+
+    }
+}
